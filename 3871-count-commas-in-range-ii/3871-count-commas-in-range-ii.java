7d7534fe3b6de3394254c980769a9bc85c66a472
Aditya Pratap Singh <adityanagvanshi61@gmail.com>
2026-09-09 22:48:35 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long countCommas(long n) {
+        long totalCommas = 0;
+        long base = 1000;
+        
+        while (n >= base) {
+            totalCommas += (n - base + 1);
+            base *= 1000;
+        }
+        
+        return totalCommas;
+    }
+}
